2d4f3c24022022d4c9abaf00ed0da6ae140ddf1b
Jared <jaredarodriguez@gmail.com>
2020-03-19 21:20:38 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express();
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('Index')
+});
+
+// About Route 
+app.get('/about', (req, res) => {
+    res.send('About')
+})
+
+const port = 5000; 
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`)
+})
+
