1e8a0291cb8c8348cde2faab1c2f246b71a53289
Jared <jaredarodriguez@gmail.com>
2020-03-19 22:12:35 -0400

    mongoDB connection

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Map global promise
+mongoose.Promise = global.Promise
+
+// Connect to mongoose 
+mongoose.connect('mongodb://localhost:27017/vidjot', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err))
+
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs());
