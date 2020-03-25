48a4f0db20927b27fcb7a16d67c3346e9574ba37
Jared <jaredarodriguez@gmail.com>
2020-03-25 15:46:22 -0400

    fetch ideas from mongodb

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
 const app = express();
@@ -17,8 +18,14 @@ require('./models/Idea');
 const Idea = mongoose.model('ideas');
 
 // Handlebars Middleware
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
+
+// Body parser middleware  
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Index Route
 app.get('/', (req, res) => {
@@ -34,11 +41,50 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/ideas', (req, res) => {
-    res.render('ideas/ideas')
+    Idea.find({})
+    .sort({date: 'desc'})
+    .then(ideas => {
+        res.render('ideas/index', {
+            ideas: ideas
+        }); 
+    })
 })
+
+// Add Idea Form 
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add')
 })
+
+// Process Form
+app.post('/ideas', (req, res) => {
+    let errors = []; 
+
+    if(!req.body.title) {
+        errors.push({text: 'Please add a title'});
+    }
+    if(!req.body.details) {
+        errors.push({text: 'Please add a details'});
+    }
+
+    if(errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors, 
+            title: req.body.title, 
+            details: req.body.details
+        });
+     } else {
+         const newUser = {
+             title: req.body.title, 
+             details: req.body.details
+         }
+            new Idea(newUser)
+                .save()
+                .then(idea => {
+                    res.redirect('/ideas')
+                })
+        }
+    });
+
 
 const port = 5000; 
 
